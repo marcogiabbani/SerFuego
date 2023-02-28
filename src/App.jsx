48dde1799e8af3939ec4ed4eb2d3362bd1469153
marco <you@example.com>
2023-02-28 18:56:03 -0300

    starts using gsap, makes first animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
+import { gsap } from 'gsap'
 
 
 function App() {
 
+  const divRef = useRef(null)
+
+  useEffect(() => {
+
+    gsap.from(divRef.current, {
+      duration: 1,
+      autoAlpha: 0,
+      ease: 'none',
+      delay: 1
+    })
+    console.log('hola')
+
+  }, [divRef])
+
   return (
-    <div>
-      <h1>Hole mundo amarillio</h1>
+    <div className='h-[500vh]'>
+      <div ref={divRef} className='h-[100vh] bg-green-500'>
+        <p className='text-center text-white text-2xl'>
+          Scroll downs
+        </p>
+      </div>
+      <div className='h-[100vh] bg-yellow-300'>2</div>
+      <div className='h-[100vh] bg-blue-600'>3</div>
+      <div className='h-[100vh] bg-slate-500'>4</div>
+      <div className='h-[100vh] bg-green-500'>5</div>
+
+
+
     </div>
   )
 }
