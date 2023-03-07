7d59f417fe22fefbcea8530f452aba9068c43e91
marco <you@example.com>
2023-03-07 10:07:17 -0300

    How to create an array of React refs to tween them individualy

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,37 @@ import React, { useRef, useEffect, useState } from 'react'
 import { gsap } from 'gsap'
 
 
+const sections = [
+  {
+    title: 'Title 1',
+    subtitle: 'Subtitle 1'
+  },
+  {
+    title: 'Title 2',
+    subtitle: 'Subtitle 2'
+  },
+  {
+    title: 'Title 3',
+    subtitle: 'Subtitle 3'
+  },
+  {
+    title: 'Title 4',
+    subtitle: 'Subtitle 4'
+  },
+  {
+    title: 'Title 5',
+    subtitle: 'Subtitle 5'
+  },
+]
+
+
 function App() {
 
   const [background, setBackground] = useState('#262626')
 
   const divRef = useRef(null)
+  const revealRefs = useRef([])
+  revealRefs.current = []
 
   const toggleBackground = () => {
     const color = background !== '#5a7d95'
@@ -35,8 +61,16 @@ function App() {
 
   }, [divRef])
 
+
+  const addToRefs = (el) => {
+    if(el && !revealRefs.current.includes(el)) {
+      revealRefs.current.push(el)
+    }
+    console.log(revealRefs.current)
+  }
+
   return (
-    <div className='h-[500vh]'>
+    <div className=''>
       <div ref={divRef} className='h-[100vh] bg-green-500'>
         <p className='text-center text-white text-2xl'>
           Scroll downs
@@ -45,12 +79,22 @@ function App() {
           onClick={() => toggleBackground()}
           className='bg-white'>toggleBackground</button>
       </div>
-      <div className='h-[100vh] bg-yellow-300'>2</div>
-      <div className='h-[100vh] bg-blue-600'>3</div>
-      <div className='h-[100vh] bg-slate-500'>4</div>
-      <div className='h-[100vh] bg-green-500'>5</div>
 
-
+      <div className='flex flex-col justify-center h-[100vh]'>
+        {
+          sections.map(({ title, subtitle }) => {
+            return (
+              <div
+                key={title}
+                ref={addToRefs}
+                className='h-32 w-40 bg-white m-auto'>
+                <h2>{title}</h2>
+                <p>{subtitle}</p>
+              </div>
+            )
+          })
+        }
+      </div>
 
     </div>
   )
