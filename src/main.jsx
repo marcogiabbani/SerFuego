b63aa56f365aa06404e053aee41ec2e8e03c5c52
marcogiabbani <mtgiabbani@gmail.com>
2023-04-30 20:12:39 -0300

    initial setup eslint, tailwindcss

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,10 @@ import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div className='min-h-screen bg-red-500'>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+
+  </div>
 )
