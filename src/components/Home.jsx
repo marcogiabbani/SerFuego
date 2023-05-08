d29eff81b7cb98837703d4a82746482cfcb09773
marcogiabbani <mtgiabbani@gmail.com>
2023-05-08 12:17:03 -0300

    cleans unused components

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,27 +1,26 @@
 import React from 'react'
 
 import NavBar from './NavBar'
-import ImageToggle from './ImageToggle'
 import Ellipse from './Ellipse'
 
 function Home() {
 
   return (
     <div className='h-screen flex flex-col justify-evenly px-16'>
+
       <div className='flex justify-between h-[15vh] mt-10 px-7'>
         <h1 className='text-[60px]'>Serfuego_</h1>
         <NavBar />
-
       </div>
 
       <div className='w-[100%] my-5'>
-        {/* <ImageToggle /> */}
         <Ellipse />
       </div>
 
       <div className='flex justify-between px-7  text-xl '>
 
         <div className='flex flex-col'>
+
           <span>-</span>
           <span>Serfuego</span>
           <span>Design & ArtDirection</span>
@@ -35,11 +34,14 @@ function Home() {
             <span>  I  </span>
             <span>  T  </span>
           </div>
+
         </div>
+
         <div className='flex flex-col justify-between'>
           <span></span>
           <span className='text-[30px]'>SF ©</span>
         </div>
+
       </div>
 
     </div>
