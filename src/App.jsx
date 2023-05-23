4aec190b339edcaac842e5471aafe8a6fa1d73cd
marcogiabbani <mtgiabbani@gmail.com>
2023-05-22 22:10:52 -0300

    adds weird way of media query

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,29 @@ import Contact from './components/Contact';
 import Installation from './components/Installation';
 
 function App() {
-
-  return (
-
-    <HashRouter>
-      <div className='h-[99vh] flex flex-col justify-between'>
-        <div>
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route exact path="/contact" element={<Contact />} />
-            <Route exact path="/installation" element={<Installation />} />
-
-          </Routes>
-        </div>
+  const isMobile = window.innerWidth < 768; // Define the mobile breakpoint
+  return isMobile
+    ? (
+      <div >
+        <span>mobile</span>
       </div>
-    </HashRouter>
-  )
+    )
+    : (
+
+      <HashRouter>
+
+        <div className='h-[99vh] flex flex-col justify-between'>
+          <div>
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/contact" element={<Contact />} />
+              <Route exact path="/installation" element={<Installation />} />
+
+            </Routes>
+          </div>
+        </div>
+      </HashRouter>
+    )
 }
 
 export default App
