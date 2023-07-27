77962315689ae27defe092455601aeab6968960a
marcogiabbani <mtgiabbani@gmail.com>
2023-07-27 17:18:54 -0300

    ends contact mobile in single component (MobileContact)

--- src/components/MobileContact.jsx
+++ src/components/MobileContact.jsx
@@ -2,31 +2,56 @@ import React from 'react'
 import MobileFooter from './MobileFooter'
 
 function MobileContact() {
-  return (
-    <div className='bg-[#FF0000] px-2 '>
-      <div className='h-[85vh] mt-0 flex flex-col justify-between'>
-        <div className='h-full flex flex-col text-4xl m-auto'>
-          <span className='pb-3'>Francisco Matte</span>
-          <span className='pb-3'>Design & ArtDirection</span>
-          <span className='pb-3'>(+54) 11 3420 0305</span>
-          <span className='pb-3'>fuego@serfuego.com</span>
-          <span className='pb-3'>@serfuego</span>
-          <span className='pb-3'>born in Barcelona_</span>
-        </div>
-        <div className='h-full flex flex-col justify-between'>
-          <div className='text-center h-full'>
-            <span className=''>
-              The phenomenon of combustion manifested in light, flame and heat.
-            </span>
-          </div>
-          <div className='h-full'>
-            <MobileFooter />
-          </div>
+    return (
+        <div className='bg-[#FF0000] h-[85vh]
+            md:bg-black md:h-screen md:text-[#FF0000]'>
+
+            <div className='h-full mt-0 flex flex-col justify-between
+                md:h-screen'>
+
+                <div className='h-full flex flex-col text-4xl m-auto
+                    md:text-9xl md:px-24 md:pt-16'>
+                    <span className='pb-3 
+                        md:pb-0 md:max-h-[110px] md:text-[150px]'>Francisco Matte</span>
+                    <span className='pb-3
+                        md:pb-0 md:max-h-[110px] md:text-[150px]'>Design & ArtDirection</span>
+                    <span className='pb-3
+                        md:pb-0 md:max-h-[110px] md:text-[150px]'>(+54) 11 3420 0305</span>
+                    <span className='pb-3
+                        md:pb-0 md:max-h-[110px] md:text-[150px]'>fuego@serfuego.com</span>
+                    <span className='pb-3
+                        md:pb-0 md:max-h-[110px] md:text-[150px]'>@serfuego</span>
+                    <span className='pb-3
+                        md:pb-0 md:max-h-[110px] md:text-[150px]'>born in Barcelona_</span>
+                </div>
+
+                <div className='h-full max-h-[60px] flex flex-col justify-between
+                    md:px-24 md:max-h-full'>
+                        
+                    <div className='text-center h-full md:invisible'>
+                        <span className=''>
+                            The phenomenon of combustion manifested in light, flame and heat.
+                        </span>
+                    </div>
+
+                    <div className='invisible md:visible h-full '>
+
+                        <span className='h-[110px] text-[40px] align-baseline'> The phenomenon of combustion manifested in light, flame and heat.</span>
+                        <svg viewBox="0 0 100 1" xmlns="http://www.w3.org/2000/svg">
+                            <line x1="0" y1="0" x2="100" y2="0" stroke="red" strokeWidth="0.1" />
+                        </svg>
+                    </div>
+
+
+                </div>
+                <div className='md:px-16'>
+                    <MobileFooter />
+                </div>
+
+            </div>
 
         </div>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default MobileContact
--- src/components/MobileFooter.jsx
+++ src/components/MobileFooter.jsx
@@ -2,49 +2,49 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 function MobileFooter() {
-  return (
-    <div className='flex flex-col justify-between text-xl 
+    return (
+        <div className='flex flex-col justify-between text-xl 
       md:text-[30px]'>
 
-      <div className='flex flex-col justify-between text-center
+            <div className='flex flex-col justify-between text-center
         md:flex-row md:text-left'>
 
-        <div className='w-40 h-full m-auto flex flex-col justify-between 
+                <div className='w-40 h-full m-auto flex flex-col justify-between 
           md:h-auto md:m-0 md:flex-row'>
-          <div className='pb-5'>
-            <Link to='https://franciscoluismatte.com/' target="_blank">
-              <span >  F  </span>
-            </Link>
-          </div>
-          <div className='pb-5'>
-            <Link to='https://www.instagram.com/barthes.vhs/' target="_blank">
-              <span>  I  </span>
-            </Link>
-          </div>
-          <div className='pb-5'>
-            <Link to='https://www.behance.net/franmatte' target="_blank">
-              <span>  B  </span>
-            </Link>
-          </div>
-          <div className='pb-5'>
-            <Link to='https://romantiv.com/' target="_blank">
-              <span>  R  </span>
-            </Link>
-          </div>
-        </div>
-        <div>
+                    <div className='pb-5 md:p-0'>
+                        <Link to='https://franciscoluismatte.com/' target="_blank">
+                            <span >  F  </span>
+                        </Link>
+                    </div>
+                    <div className='pb-5 md:p-0'>
+                        <Link to='https://www.instagram.com/barthes.vhs/' target="_blank">
+                            <span>  I  </span>
+                        </Link>
+                    </div>
+                    <div className='pb-5 md:p-0'>
+                        <Link to='https://www.behance.net/franmatte' target="_blank">
+                            <span>  B  </span>
+                        </Link>
+                    </div>
+                    <div className='pb-5 md:p-0'>
+                        <Link to='https://romantiv.com/' target="_blank">
+                            <span>  R  </span>
+                        </Link>
+                    </div>
+                </div>
+                <div>
 
-          <Link to='/'>
-            <div className='text-5xl pt-4
+                    <Link to='/'>
+                        <div className='text-5xl pt-4
               md:text-[30px] md:p-0'>
-              <span >SF ©</span>
-            </div>
-          </Link>
-        </div>
+                            <span >SF ©</span>
+                        </div>
+                    </Link>
+                </div>
 
-      </div>
-    </div>
-  )
+            </div>
+        </div>
+    )
 }
 
 export default MobileFooter
